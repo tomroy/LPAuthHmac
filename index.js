81afb52b2ad17c2587cdd8efc189996d9f0ed93e
Tom Lin <tom.lin@linecorp.com>
2020-06-04 18:17:53 +0800

    add generate nonce function

--- index.js
+++ index.js
@@ -30,3 +30,15 @@ function generate() {
   document.getElementById("headers").innerHTML =
     authorizationNonce + " " + authorization;
 }
+
+function generate_nonce() {
+  document.getElementById("nonce").value = uuidv4();
+}
+
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
